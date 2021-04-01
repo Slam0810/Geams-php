7c8f985ae1d1199683ca495695fc3aeadbffd72c
Birama0810 <birama.tour@gmail.com>
2021-04-01 09:02:10 +0200

    Creation des controller + la methode request

--- src/Controller/AdvertController.php
+++ src/Controller/AdvertController.php
@@ -2,14 +2,36 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
 use Twig\Environment;
+use Symfony\Component\HttpFoundation\Request;
 
-class AdvertController
+class AdvertController extends AbstractController
 {
-    public function index(Environment $twig)
+    /**
+    * @Route("/advert", name="oc_advert_index")
+    */
+    public function index(RouterInterface $router)
     {
-        $content = $twig->render('Advert/index.html.twig', ['name' => 'Aisha']) ;
-        return new Response($content);
+        $url = $this->generateUrl(
+            'oc_advert_view',
+            ['id' => 5]
+        );
+        return new Response("L'URL de l'annonce d'id 5 est ".$url);
+    }
+    public function view($id, Request $request)
+    {
+        $tag = $request->query->get('tag');
+        return new Response("Affichage de l'annonce d'id : ".$id.", avec le tag :" .$tag);
+    }
+    public function viewSlug($slug, $year, $format)
+    {
+        return new Response(
+            "On pourrait afficher l'annonce correspondant au
+    slug '".$slug."', créée en ".$year." et au format ".$format."."
+        );
     }
 }
